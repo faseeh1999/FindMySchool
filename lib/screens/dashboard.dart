3bb54a93a25cb3101351b5bfb9ce3bdf4abb7f4f
Faseeh PC <faseehshahzad.fs@gmail.com>
2021-03-15 18:27:35 +0500

    Added Bottom Navigation Bar
    Only Switching between different containers.

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,37 +1,161 @@
+import 'package:FindMySchool/theme/appbar.dart';
+import 'package:FindMySchool/theme/text.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'login.dart';
 
 class Dashboard extends StatefulWidget {
+
   @override
   _DashboardState createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
+
+  int _currentIndex = 0;
+  PageController _pageController;
+
+  List listOfColors = [
+    Container(
+      color: Colors.red,
+
+    ),
+    Container(
+      color: Colors.blueAccent,
+
+    ),
+    Container(
+      color: Colors.amber,
+
+    ),
+    Container(
+      color: Colors.green,
+
+    ),
+    Container(
+      color: Colors.brown,
+      child: Center(
+        child: FlatButton(
+          child: Text("Logout"),
+          textColor: Colors.white,
+        ),
+      ),
+
+
+    ),
+  ];
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello"),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.logout,
+              color: Colors.black,
+            ),
+            onPressed: () async {
+              try {
+                await FirebaseAuth.instance.signOut();
+              } catch (e) {
+                print(e);
+              }
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                      (route) => false);
+            },
+          )
+        ],
+
+        leading: Icon(Icons.menu_sharp, color: Colors.black,),
+
+        elevation: 0.0,
+
+        title: Text("Home", style: appBarTitle,),
         centerTitle: true,
 
       ),
-      body: Center(child: FlatButton(
-        color: Colors.blueAccent,
-        child: Text("Logout"),
-        onPressed: () async {
-          try {
-            await FirebaseAuth.instance.signOut();
-          } catch (e) {
-            print(e);
-          }
-          Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (context) => LoginScreen()),
-                  (route) => false);
-        },
-      )),
-    );
+       bottomNavigationBar: buildBottomNavyBar(),
+
+       body: listOfColors[_currentIndex]
+      );
+
+  }
+
+  BottomNavyBar buildBottomNavyBar() {
+    return BottomNavyBar(
+      selectedIndex: _currentIndex,
+      showElevation: true, // use this to remove appBar's elevation
+      onItemSelected: (index) => setState(() {
+        _currentIndex = index;
+        // _pageController.animateToPage(index,
+        //     duration: Duration(milliseconds: 300), curve: Curves.ease);
+      }),
+       curve: Curves.easeIn,
+
+
+       items: <BottomNavyBarItem>[
+         BottomNavyBarItem(
+           textAlign: TextAlign.justify,
+             icon: Icon(Icons.search),
+             title: Text("Search", style: TextStyle(fontFamily: 'ss'),),
+           activeColor: Colors.red,
+           inactiveColor: Colors.grey,
+
+         ),
+         BottomNavyBarItem(
+            textAlign: TextAlign.justify,
+             icon: Icon(Icons.star_border_outlined),
+             title: Text("Bookmarks", style: TextStyle(fontFamily: 'ss'),),
+           activeColor: Colors.blueAccent,
+           inactiveColor: Colors.grey,
+         ),
+         BottomNavyBarItem(
+           textAlign: TextAlign.justify,
+           icon: Icon(Icons.home_sharp),
+           title: Text("Home", style: TextStyle(fontFamily: 'ss'),),
+           activeColor: Colors.amber,
+           inactiveColor: Colors.grey,
+         ),
+         BottomNavyBarItem(
+           textAlign: TextAlign.justify,
+           icon: Icon(Icons.notifications_active),
+           title: Text("Notifications", style: TextStyle(fontFamily: 'ss'),),
+           activeColor: Colors.green,
+           inactiveColor: Colors.grey,
+         ),
+         BottomNavyBarItem(
+           textAlign: TextAlign.justify,
+           icon: Icon(Icons.settings),
+           title: Text("Settings", style: TextStyle(fontFamily: 'ss'),),
+           activeColor: Colors.brown,
+           inactiveColor: Colors.grey,
+         ),
+       ],
+     );
   }
 }
---
+++ lib/theme/appbar.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const appBarTitle = TextStyle(fontFamily: 'ss', color: Colors.black,fontWeight: FontWeight.normal, fontSize: 18);
